9ea1bcbb4c0e283947c021c9396be9d76858d0cd
riven <4771215@qq.com>
2021-08-04 16:07:19 +0800

    dma serial ping pong works~

--- samples/Serial/main.cpp
+++ samples/Serial/main.cpp
@@ -12,11 +12,15 @@
 
 using namespace codal;
 
+#define APP_ID  100
+#define APP_TX_DONE 101
+#define APP_RX_DONE 102
+
 PiPico pico;
 ZSingleWireSerial * sws;
-uint8_t rxbuff[256];
 
-char * txt = "hello world";
+uint8_t txBuff[256];
+uint8_t rxBuff[256];
 
 void print_buf(const uint8_t *buf, size_t len) {
     for (size_t i = 0; i < len; ++i) {
@@ -28,8 +32,16 @@ void print_buf(const uint8_t *buf, size_t len) {
     }
 }
 
-void sws_done(uint16_t errCode) {
-
+void sws_done(uint16_t code) {
+    switch (code)
+    {
+    case SWS_EVT_DATA_SENT:
+        Event(APP_ID, APP_TX_DONE);
+        break;
+    case SWS_EVT_DATA_RECEIVED:
+        Event(APP_ID, APP_RX_DONE);
+        break;
+    }
 
 }
 
@@ -37,25 +49,26 @@ int main() {
     float abc = 123.0;
     printf("rom %d %02f\r\n", rp2040_rom_version(), abc);
 
+    for (int i=0;i<256;i++){
+        txBuff[i] = i;
+    }
+
     pico.init();
     sws = new ZSingleWireSerial(pico.io.GP9);
-#if 0
-    while(1) {
-        sws->sendDMA((uint8_t *)txt, strlen(txt));
-        pico.sleep(1);
-        sws->abortDMA();
-        sws->p.setDigitalValue(0);
-        pico.sleep(2);
-        sws->p.setDigitalValue(1);
-        pico.sleep(2000);
-    }
-#else
-    memset(rxbuff, 0, 256);
-    sws->receiveDMA(rxbuff, 256);
+    
     sws->setIRQ(sws_done);
-    while(1){
-        pico.sleep(2000);
-        print_buf(rxbuff, 256);
+    while (1)
+    {
+        sws->sendDMA(txBuff, 256);
+        fiber_wait_for_event(APP_ID, APP_TX_DONE);
+        sws->abortDMA();
+        printf("ping >>>\r\n");
+        pico.sleep(3000);
+        
+        memset(rxBuff,0,256);
+        sws->receiveDMA(rxBuff, 256);
+        fiber_wait_for_event(APP_ID, APP_RX_DONE);
+        printf("<<< pong\r\n");
+        print_buf(rxBuff, 256);
     }
-#endif
 }
